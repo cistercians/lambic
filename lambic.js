02da548894f973c7231a948d92e8f41e98d3f3ff
Johan <jonargyne@gmail.com>
2021-02-01 00:15:53 -0800

    serf spawning

--- lambic.js
+++ lambic.js
@@ -1136,8 +1136,8 @@ if(saveMap){
 // day/night cycle
 tempus = 'XII.a';
 period = 120; // 1=1hr, 2=30m, 4=15m, 12=5m, 60=1m, 120=30s, 360=10s (number of game days per 24 hours)
-var cycle = ['XII.a','I.a','II.a','III.a','IV.a','V.a','VI.a','VII.a','VIII.a','IX.a','X.a','XI.a'
-            ,'XII.p','I.p','II.p','III.p','IV.p','V.p','VI.p','VII.p','VIII.p','IX.p','X.p','XI.p'];
+var cycle = ['XII.a','I.a','II.a','III.a','IV.a','V.a','VI.a','VII.a','VIII.a','IX.a','X.a',
+'XI.a','XII.p','I.p','II.p','III.p','IV.p','V.p','VI.p','VII.p','VIII.p','IX.p','X.p','XI.p'];
 var tick = 1;
 day = 0;
 
